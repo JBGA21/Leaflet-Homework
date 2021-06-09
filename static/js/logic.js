17bf2c77d6178cc3cbb025c878aac327d82bc8a3
JBGA21 <jesusbeltran_93@hotmail.com>
2021-06-09 17:08:18 -0500

    function createMap added

--- static/js/logic.js
+++ static/js/logic.js
@@ -38,3 +38,12 @@ function createFeatures(earthquakeData) {
 
     createMap(earthquakes);
 }
+
+function createMap(earthquakes) {
+
+    var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+        maxZoom: 18,
+        id: "mapbox.light",
+        accessToken: "pk.eyJ1Ijoia2FyZW5lbWNnZWUiLCJhIjoiY2sweDMzcnFmMDJmZTNjb2M2bTA0bDBsYiJ9.2VPrGXusLoJpCMfTWrgBRQ"
+    });
